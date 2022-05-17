07d01ac9b1f3da64e9713ed88c4d098db4c2446c
Jorgeeperalta <edudela36@gmail.com>
2022-05-17 14:09:39 -0300

    add map vuelayers

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueLayers from 'vuelayers'
+import 'vuelayers/dist/vuelayers.css' // needs css-loader
+
+Vue.use(VueLayers)
 
 Vue.config.productionTip = false
 
